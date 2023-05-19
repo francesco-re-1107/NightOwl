93f75454b002c4aa9a3a4f6d2598ab734bcd0635
Francesco Re <francesco.re1107@gmail.com>
2023-05-19 18:30:29 +0200

    Change resnet50 to resnet18 and other improvements

--- src/model.py
+++ src/model.py
@@ -1,21 +1,20 @@
 import torch.nn as nn
 import torchvision.models as models
+from normalize import Normalize
 
 class Model(nn.Module):
 
-    def __init__(self):
-        self.resnet50 = models.resnet50(weights='ResNet50_Weights.DEFAULT')
+    def __init__(self, means, stds):
+        self.normalize = Normalize(means, stds)
 
-        for param in self.resnet50.parameters():
-            param.requires_grad = False
-        
-        self.resnet50.fc = nn.Sequential(
-            nn.Linear(2048, 256),
+        self.resnet18 = models.resnet18()
+        self.resnet18.fc = nn.Sequential(
+            nn.Linear(self.resnet18.fc.in_features, 256),
             nn.ReLU(),
             nn.Dropout(0.4),
-            nn.Linear(256, 3),
+            nn.Linear(256, 3), 
             nn.LogSoftmax(dim=1)
         )
 
     def forward(self, x):
-        return self.resnet50(x)
+        return self.resnet18(self.normalize(x))
